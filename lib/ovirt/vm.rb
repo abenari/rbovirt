65fc6fb334506092fa1d399478460056e3e1bf99
Amos Benari <abenari@redhat.com>
2012-01-30 11:05:37 +0200

    fixed selecting template and cluster by id in creating new vm.

--- lib/ovirt/vm.rb
+++ lib/ovirt/vm.rb
@@ -29,22 +29,18 @@ module OVIRT
       builder = Nokogiri::XML::Builder.new do
         vm{
           name_ opts[:name] || "i-#{Time.now.to_i}"
-          template_{
-            if opts[:template]
-              id_(opts[:template])
-            elsif opts[:template_name]
-              name_(opts[:template_name])
-            else
-              name_('Blank')
-            end
-          }
-          cluster_{
-            if opts[:cluster]
-              id_(opts[:cluster])
-            elsif opts[:cluster_name]
-              name_(opts[:cluster_name])
-            end
-          }
+          if opts[:template]
+            template_ :id => (opts[:template])
+          elsif opts[:template_name]
+            template_{ name_(opts[:template_name])}
+          else
+            template_{name_('Blank')}
+          end
+          if opts[:cluster]
+            cluster_( :id => opts[:cluster])
+          elsif opts[:cluster_name]
+            cluster_{ name_(opts[:cluster_name])}
+          end
           type_ opts[:hwp_id] || 'Server'
           memory opts[:hwp_memory] ? (opts[:hwp_memory].to_i*1024*1024).to_s : (512*1024*1024).to_s
           cpu {
