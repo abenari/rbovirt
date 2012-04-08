6294c8bd5c22984678b02828115aebf95e725cd7
Amos Benari <abenari@redhat.com>
2012-04-08 16:52:31 +0300

    added filter storage domain by role

--- lib/client/storage_domain_api.rb
+++ lib/client/storage_domain_api.rb
@@ -8,8 +8,10 @@ module OVIRT
     def storagedomains(opts={})
       search= opts[:search] ||''
       http_get("/storagedomains?search=%s" % CGI.escape(search)).xpath('/storage_domains/storage_domain').collect do |sd|
-        OVIRT::StorageDomain::new(self, sd)
-      end
+        storage_domain = OVIRT::StorageDomain::new(self, sd)
+        #filter by role is not supported by the search language. The work around is to list all then filter.
+        (opts[:role].nil? || storage_domain.role == opts[:role]) ? storage_domain : nil
+      end.compact
     end
   end
 end
