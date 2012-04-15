34c209f535367ae45f0b3b5577c31a8a34dbea5f
Amos Benari <abenari@redhat.com>
2012-04-15 13:34:55 +0300

    by default list storage domains of the current datacenter.

--- lib/client/storage_domain_api.rb
+++ lib/client/storage_domain_api.rb
@@ -6,10 +6,10 @@ module OVIRT
     end
 
     def storagedomains(opts={})
-      search= opts[:search] ||''
+      search= opts[:search] || ("datacenter=%s" % current_datacenter.name)
       http_get("/storagedomains?search=%s" % CGI.escape(search)).xpath('/storage_domains/storage_domain').collect do |sd|
         storage_domain = OVIRT::StorageDomain::new(self, sd)
-        #filter by role is not supported by the search language. The work around is to list all then filter.
+        #filter by role is not supported by the search language. The work around is to list all, then filter.
         (opts[:role].nil? || storage_domain.role == opts[:role]) ? storage_domain : nil
       end.compact
     end
