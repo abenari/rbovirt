649d414ca36e0857a3fdf5ba53352c7d200a2d9f
Lukas Zapletal <lzap+git@redhat.com>
2016-03-11 14:54:37 +0100

    Fixes Blank template display

--- lib/ovirt/template.rb
+++ lib/ovirt/template.rb
@@ -39,8 +39,8 @@ module OVIRT
       @profile = (xml/'type').first.text
       @cluster = Link::new(@client, (xml/'cluster').first[:id], (xml/'cluster').first[:href])
       @display = {
-        :type => (xml/'display/type').first.text,
-        :monitors => (xml/'display/monitors').first.text
+        :type => (xml/'display/type').first.text rescue '',
+        :monitors => (xml/'display/monitors').first.text rescue 0
       }
       @cores = ((xml/'cpu/topology').first[:cores].to_i * (xml/'cpu/topology').first[:sockets].to_i rescue nil)
       @storage = ((xml/'disks/disk/size').first.text rescue nil)
--- lib/ovirt/vm.rb
+++ lib/ovirt/vm.rb
@@ -306,12 +306,12 @@ module OVIRT
       @host = Link::new(@client, (xml/'host').first[:id], (xml/'host').first[:href]) rescue nil
       @cluster = Link::new(@client, (xml/'cluster').first[:id], (xml/'cluster').first[:href])
       @display = {
-        :type => (xml/'display/type').first.text,
+        :type => (xml/'display/type').first.text rescue '',
         :address => ((xml/'display/address').first.text rescue nil),
         :port => ((xml/'display/port').first.text rescue nil),
         :secure_port => ((xml/'display/secure_port').first.text rescue nil),
         :subject => ((xml/'display/certificate/subject').first.text rescue nil),
-        :monitors => (xml/'display/monitors').first.text
+        :monitors => (xml/'display/monitors').first.text rescue 0
       }
       @cores = ((xml/'cpu/topology').first[:cores].to_i * (xml/'cpu/topology').first[:sockets].to_i rescue nil)
       @storage = ((xml/'disks/disk/size').first.text rescue nil)
