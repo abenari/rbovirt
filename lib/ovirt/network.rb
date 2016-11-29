76c70e64b1ca2c223bd8c83081e3af6a0471db44
Baptiste Agasse <baptiste.agasse@lyra-network.com>
2016-11-29 12:04:36 +0100

    Add mtu vlan_id and usages to networks

--- lib/ovirt/network.rb
+++ lib/ovirt/network.rb
@@ -1,6 +1,6 @@
 module OVIRT
  class Network < BaseObject
-    attr_reader :description, :datacenter, :cluster, :stp, :status
+    attr_reader :description, :datacenter, :cluster, :vlan_id, :mtu, :stp, :usages, :status
 
     def initialize(client, xml)
       super(client, xml[:id], xml[:href], (xml/'name').first.text)
@@ -13,6 +13,9 @@ module OVIRT
     def parse_xml_attributes!(xml)
       @description = ((xml/'description').first.text rescue nil)
       @stp = ((xml/'stp').first.text rescue false)
+      @mtu = ((xml/'mtu').first.text rescue nil)
+      @vlan_id = ((xml/'vlan').first[:id] rescue nil)
+      @usages = ((xml/'usages/usage').collect{ |usage| usage.text } rescue [])
       @datacenter = Link::new(@client, (xml/'data_center').first[:id], (xml/'data_center').first[:href]) unless (xml/'data_center').empty?
       @cluster = Link::new(@client, (xml/'cluster').first[:id], (xml/'cluster').first[:href]) unless (xml/'cluster').empty?
       @status = ((xml/'status/state').first.text rescue nil)
